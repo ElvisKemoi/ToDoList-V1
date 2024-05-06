76c21577aff37ca399a5e71d907f23142cd52ce6
Elvis Kemoi <elviskemoi001@gmail.com>
2024-05-06 20:00:47 +0300

    End of To Do List V1 Start of To Do List V2

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+// const mongoose = require("mongoose ");
 
-const date = require(__dirname+"/date.js")
-
-
-
+const date = require(__dirname + "/date.js");
 
 const workItems = [];
 const list = ["Buy food.", "Cook food.", "Eat food.", "clearn dishes"];
@@ -16,30 +14,29 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    let day = date.getDate();
-    res.render("list", { listTitle: day, newListItems: list });
-    
+	let day = date.getDate();
+	res.render("list", { listTitle: day, newListItems: list });
 });
 
 app.get("/work", (req, res) => {
-    res.render("list", { listTitle: "Work", newListItems: workItems });
+	res.render("list", { listTitle: "Work", newListItems: workItems });
 });
 
-app.get("/about", (req,res)=>{
-    res.render("about");
-})
+app.get("/about", (req, res) => {
+	res.render("about");
+});
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        list.push(item);
-        res.redirect("/");
-    }
+	let item = req.body.newItem;
+	if (req.body.list === "Work") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		list.push(item);
+		res.redirect("/");
+	}
 });
 
 app.listen(3000, () => {
-    console.log("Server is live on port 3000");
+	console.log("Server is live on port 3000");
 });
