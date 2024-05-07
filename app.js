ab9c0219802ec13068c701e81d9803517955d535
Elvis Kemoi <elviskemoi001@gmail.com>
2024-05-07 16:17:09 +0300

    Continuing with the todolist v2

--- app.js
+++ app.js
@@ -3,19 +3,60 @@ const app = express();
 const bodyParser = require("body-parser");
 // const mongoose = require("mongoose ");
 
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
-const workItems = [];
-const list = ["Buy food.", "Cook food.", "Eat food.", "clearn dishes"];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = mongoose.Schema({
+	name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+	name: "Welcome to your todo list",
+});
+const item2 = new Item({
+	name: "Hit the + button to add an item.",
+});
+const item3 = new Item({
+	name: "<-- Hit this checkbox to delete ",
+});
+
+const defaultItems = [item1, item2, item3];
+
+async function saveItems() {
+	try {
+		const result = await Item.insertMany(defaultItems);
+		console.log(result);
+	} catch (error) {
+		console.error("Error saving items:", error);
+	}
+}
+
+async function getTheList() {
+	const items = await Item.find();
+	console.log(items);
+}
+// getTheList();
+
+// saveItems();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-	let day = date.getDate();
-	res.render("list", { listTitle: day, newListItems: list });
+app.get("/", async (req, res) => {
+	try {
+		const list = await Item.find();
+		// console.log(list);
+		res.render("list", { listTitle: "Today", newListItems: list });
+	} catch (error) {
+		console.error("There was an error reading the document" + error);
+	}
+	// const list = await Item.find();
+	// res.render("list", { listTitle: "Today", newListItems: list });
 });
 
 app.get("/work", (req, res) => {
