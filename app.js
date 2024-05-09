d817bebcce36fcaa26fb78c029c164060f43b473
Elvis Kemoi <elviskemoi001@gmail.com>
2024-05-09 17:13:21 +0300

    Finished the v2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 // const mongoose = require("mongoose ");
 
 const mongoose = require("mongoose");
@@ -25,22 +26,22 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = mongoose.Schema({
+	name: String,
+	items: [itemsSchema],
+});
+
+const List = mongoose.model("List", listSchema);
+
 async function saveItems() {
 	try {
 		const result = await Item.insertMany(defaultItems);
-		console.log(result);
+
+		console.log("Successfully saved " + defaultItems.length + " items");
 	} catch (error) {
 		console.error("Error saving items:", error);
 	}
 }
-
-async function getTheList() {
-	const items = await Item.find();
-	console.log(items);
-}
-// getTheList();
-
-// saveItems();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -50,8 +51,13 @@ app.set("view engine", "ejs");
 app.get("/", async (req, res) => {
 	try {
 		const list = await Item.find();
+		if (list.length === 0) {
+			saveItems();
+			res.redirect("/");
+		} else {
+			res.render("list", { listTitle: "Today", newListItems: list });
+		}
 		// console.log(list);
-		res.render("list", { listTitle: "Today", newListItems: list });
 	} catch (error) {
 		console.error("There was an error reading the document" + error);
 	}
@@ -59,22 +65,85 @@ app.get("/", async (req, res) => {
 	// res.render("list", { listTitle: "Today", newListItems: list });
 });
 
-app.get("/work", (req, res) => {
-	res.render("list", { listTitle: "Work", newListItems: workItems });
+app.get("/:customListName", async (req, res) => {
+	const customListName = _.capitalize(req.params.customListName);
+
+	const exists = await List.findOne({ name: customListName });
+
+	if (exists === null) {
+		const list = new List({
+			name: customListName,
+			items: defaultItems,
+		});
+		list.save();
+		console.log("Item saved");
+		res.redirect("/" + customListName);
+	} else {
+		console.log("It exists");
+		res.render("list", {
+			listTitle: exists.name,
+			newListItems: exists.items,
+		});
+	}
 });
 
 app.get("/about", (req, res) => {
 	res.render("about");
 });
 
-app.post("/", (req, res) => {
-	let item = req.body.newItem;
-	if (req.body.list === "Work") {
-		workItems.push(item);
-		res.redirect("/work");
-	} else {
-		list.push(item);
+app.post("/", async (req, res) => {
+	const itemName = req.body.newItem;
+	const listName = req.body.list;
+	const item = new Item({
+		name: itemName,
+	});
+	if (listName === "Today") {
+		item.save();
 		res.redirect("/");
+	} else {
+		const foundList = await List.findOne({ name: listName });
+		foundList.items.push(item);
+		foundList.save();
+		res.redirect("/" + listName);
+	}
+
+	// const item = new Item({
+	// 	name: itemName,
+	// });
+	// try {
+	// 	item
+	// 		.save()
+	// 		.then(console.log("One document was inserted into the database"));
+	// } catch (error) {
+	// 	console.errror("Error: " + error);
+	// }
+
+	// res.redirect("/");
+});
+
+app.post("/delete", async (req, res) => {
+	const checkedItemId = req.body.checkbox;
+	const listName = req.body.listName;
+	console.log(listName, checkedItemId);
+	if (listName === "Today") {
+		try {
+			await Item.findByIdAndDelete(checkedItemId);
+			console.log("The Item was removed from the database");
+			res.redirect("/");
+		} catch (error) {
+			console.error("Error: " + error);
+		}
+	} else {
+		try {
+			await List.findOneAndUpdate(
+				{ name: listName },
+				{ $pull: { items: { _id: checkedItemId } } }
+			);
+			console.log("Item removed successfully");
+			res.redirect(`/${listName}`);
+		} catch (error) {
+			console.error("Error removing item:", error);
+		}
 	}
 });
 
